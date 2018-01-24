970b43b97d942a2061f190ab392114c90630a946
Haley Mendoza <mendozahaley@gmail.com>
2018-01-23 19:41:04 -0800

    Created objects for each location with starting numbers.

---
+++ js/app.js
@@ -0,0 +1,52 @@
+// function getRandomCustomer(max) {
+//   return Math.floor(Math.random()) * Math.floor(max));
+// }
+
+var firstPike = {
+  name: '1st and Pike', 
+  minCustomers: 23,
+  maxCustomers: 65,
+  avgOrder: 6.3,
+  customersHourly: function getRandomCustomer(maxCustomers) {
+    return Math.floor(Math.random() * Math.floor(maxCustomers));
+  }
+};
+
+var seaTac = {
+  name: 'First and Pike', 
+  minCustomers: 3,
+  maxCustomers: 24,
+  avgOrder: 1.2,
+  customersHourly: function() {
+    
+  }
+};
+
+var seattleCenter = {
+  name: 'First and Pike', 
+  minCustomers: 11,
+  maxCustomers: 38,
+  avgOrder: 3.7,
+  customersHourly: function() {
+    
+  }
+};
+
+var capHill = {
+  name: 'First and Pike', 
+  minCustomers: 20,
+  maxCustomers: 38,
+  avgOrder: 2.3,
+  customersHourly: function() {
+    
+  }
+};
+
+var alki = {
+  name: 'First and Pike', 
+  minCustomers: 2,
+  maxCustomers: 16,
+  avgOrder: 4.6,
+  customersHourly: function() {    
+  }
+};
